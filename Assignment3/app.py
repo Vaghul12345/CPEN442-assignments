693db53918b9fc872e2d1e7ec35d692aba2f4b55
Vaghul12345 <vaghul12345@gmail.com>
2024-03-14 23:20:57 -0700

    Committed both protocol.py and app.py

--- Assignment3/app.py
+++ Assignment3/app.py
@@ -1,4 +1,5 @@
 # system imports
+import os
 import sys
 import socket
 from threading import Thread
@@ -9,6 +10,9 @@ from tkinter import messagebox
 
 # local import from "protocol.py"
 from protocol import Protocol
+
+# Added for Assignment 3
+import json
 
 
 class Assignment3VPN:
@@ -36,7 +40,8 @@ class Assignment3VPN:
         self.mode = None
         self.hostName = None
         self.port = None
-        self.sharedSecret = None
+        # For an actual shared secret, this would be handled properly and shared before hand. However, due to the small scale nature, I have it auto loaded
+        self.sharedSecret = bytes.fromhex("44d10b8638e5ab61d1c7429aa85a907fb87351050dc97e0f6d1e0cd9cfc96812")
         self.textMessage = None
         builder.import_variables(self, ['mode', 'hostName', 'port', 'sharedSecret', 'textMessage'])               
         builder.connect_callbacks(self)
@@ -51,7 +56,8 @@ class Assignment3VPN:
         self.receive_thread = Thread(target=self._ReceiveMessages, daemon=True)
         
         # Creating a protocol object
-        self.prtcl = Protocol()
+        self.prtcl = Protocol(shared_secret=self.sharedSecret, callback_key=self.callback_key, callback_send=self._SendMessage)
+
      
     # Distructor     
     def __del__(self):
@@ -74,7 +80,9 @@ class Assignment3VPN:
     # Handle sever mode selection
     def ServerModeSelected(self):
         pass
-
+    
+    def generate_shared_secret(length=32):
+        return os.urandom(length)
 
     # Create a TCP connection between the client and the server
     def CreateConnection(self):
@@ -134,55 +142,144 @@ class Assignment3VPN:
         except Exception as e:
             self._AppendLog("SERVER: Accepting connection failed: {}".format(str(e)))
             return False
-
+    def callback_key(self):
+        self.secureButton["state"] = "normal"
+        self.sendButton["state"] = "normal"
 
     # Receive data from the other party
+
+
     def _ReceiveMessages(self):
+        print("recieving messages")
         while True:
             try:
-                # Receiving all the data
-                cipher_text = self.conn.recv(4096)
+                raw_data = self.conn.recv(4096)
+                print("received message")
 
-                # Check if socket is still open
-                if cipher_text == None or len(cipher_text) == 0:
-                    self._AppendLog("RECEIVER_THREAD: Received empty message")
+                if not raw_data:
+                    self._AppendLog("RECEIVER_THREAD: Received empty message, nothing here to see")
                     break
 
-                # Checking if the received message is part of your protocol
-                # TODO: MODIFY THE INPUT ARGUMENTS AND LOGIC IF NECESSARY
-                if self.prtcl.IsMessagePartOfProtocol(cipher_text):
-                    # Disabling the button to prevent repeated clicks
-                    self.secureButton["state"] = "disabled"
-                    # Processing the protocol message
-                    self.prtcl.ProcessReceivedProtocolMessage(cipher_text)
-
-                # Otherwise, decrypting and showing the messaage
-                else:
-                    plain_text = self.prtcl.DecryptAndVerifyMessage(cipher_text)
-                    self._AppendMessage("Other: {}".format(plain_text.decode()))
+                try:
+                    print("got here")
+                    message = json.loads(raw_data.decode('utf-8'))
+                    print("stuck here?")
+                    if self.prtcl.IsMessagePartOfProtocol(message) and message.get('type') == 'init':
+                        print("processing initialization message")
+                        self.prtcl.ProcessReceivedProtocolMessage(message)
+                    else:
+                        self._AppendLog("RECEIVER_THREAD: Received message is not initialization type or not part of protocol")
+                except:
+                    # If connection is secure, attempt to decrypt; otherwise, log an error
+                    print("got to here, before checking if connection is secure")
                     
+    
+                    print("attempting to decrypt message")
+                    try:
+                            print("decrypt 1")
+                            decrypted_message = self.prtcl.DecryptAndVerifyMessage(raw_data)
+                            print("decrypt 2")
+                            message = json.loads(decrypted_message)
+                            if self.prtcl.IsMessagePartOfProtocol(message):
+                                print("processing protocol message")
+                                self.prtcl.ProcessReceivedProtocolMessage(message)
+                            else:
+                                self._AppendMessage(f"Other: {decrypted_message}")
+                    except Exception as e:
+                            self._AppendLog(f"RECEIVER_THREAD: Error decrypting or processing message: {e}")
+                    else:
+                        self._AppendLog("RECEIVER_THREAD: Received encrypted message but connection is not secure.")
             except Exception as e:
-                self._AppendLog("RECEIVER_THREAD: Error receiving data: {}".format(str(e)))
-                return False
+                self._AppendLog(f"RECEIVER_THREAD: General error receiving data: {e}")
+        # while True:
+        #     try:
+        #         # Receiving all the data
+        #         cipher_text = self.conn.recv(4096)
+        #         print("recieved message")
+        #         # Check if socket is still open
+        #         if cipher_text == None or len(cipher_text) == 0:
+        #             self._AppendLog("RECEIVER_THREAD: Received empty message nothing here to see")
+        #             break
+
+        #         # Checking if the received message is part of your protocol
+        #         # TODO: MODIFY THE INPUT ARGUMENTS AND LOGIC IF NECESSARY
+        #         print("recieved message 2")
+        #         try:
+
+        #             message = json.loads(cipher_text.decode('utf-8'))
+        #             if self.prtcl.IsMessagePartOfProtocol(message):
+        #                 print("within the protocol message part")
+        #                 # Disabling the button to prevent repeated clicks
+        #                 self.secureButton["state"] = "disabled"
+        #                 # Processing the protocol message
+        #                 self.prtcl.ProcessReceivedProtocolMessage(message)
+        #         except json.JSONDecodeError:
+        #         # Not a JSON, so likely an encrypted message. Continue processing below.
+        #             pass
+        #         # Otherwise, decrypting and showing the messaage
+        #         print("checking secure protocol")
+        #         if self.prtcl.isConnectionSecure:
+        #             plain_text = self.prtcl.DecryptAndVerifyMessage(message)
+        #             self._AppendMessage("Other: {}".format(plain_text))
+        #         else:
+        #             self._AppendLog("RECEIVER_THREAD: Received unsecured message.")
+        #             # plain_text = self.prtcl.DecryptAndVerifyMessage(cipher_text)
+        #             # self._AppendMessage("Other: {}".format(plain_text.decode()))
+                    
+        #     except Exception as e:
+        #         self._AppendLog("RECEIVER_THREAD: Error receiving data: {}".format(str(e)))
+        #         return False
 
 
     # Send data to the other party
     def _SendMessage(self, message):
-        plain_text = message
-        cipher_text = self.prtcl.EncryptAndProtectMessage(plain_text)
-        self.conn.send(cipher_text.encode())
+        message_type = message.get('type') if isinstance(message, dict) else None
+
+        # Convert to JSON from dictionary to fix bug
+        if isinstance(message, dict):
+            message = json.dumps(message).encode('utf-8')
+
+        if message_type == 'init':
+            print("got to sending message")
+            # encrypted_message = self.prtcl.EncryptAndProtectMessage(message)
+            self.conn.send(message)
+        elif message_type == 'key_exchange':
+            encrypted_message = self.prtcl.EncryptAndProtectMessage(message)
+            self.conn.send(encrypted_message)
+        if self.prtcl.isConnectionSecure:
+            encrypted_message = self.prtcl.EncryptAndProtectMessage(message)
+            self.conn.send(encrypted_message)
+        else:
+            self._AppendLog("MESSAGE: Cannot send. Connection not secure.")
+            # if isinstance(message, dict):
+            #     message = json.dumps(message)
+
+            # cipher_text = self.prtcl.EncryptAndProtectMessage(message)
+            # self.conn.send(cipher_text.encode())
             
 
     # Secure connection with mutual authentication and key establishment
     def SecureConnection(self):
+        print("first of secure connection")
+        init_message = self.prtcl.GetProtocolInitiationMessage()
+        # self._SendMessage(self.prtcl.GetProtocolInitiationMessage())
+        
         # disable the button to prevent repeated clicks
         self.secureButton["state"] = "disabled"
 
         # TODO: THIS IS WHERE YOU SHOULD IMPLEMENT THE START OF YOUR MUTUAL AUTHENTICATION AND KEY ESTABLISHMENT PROTOCOL, MODIFY AS YOU SEEM FIT
-        init_message = self.prtcl.GetProtocolInitiationMessage()
+        # init_message = self.prtcl.GetProtocolInitiationMessage()
+
         self._SendMessage(init_message)
 
-
+# def SecureConnection(self):
+#     try:
+#         # Starting the mutual authentication and key establishment
+#         self.prtcl.InitiateProtocol(self.sharedSecret.get())  # pass the shared secret to the protocol
+#         self.secureButton["state"] = "disabled"
+#     except Exception as e:
+#         self._AppendLog(f"SECURE_CONNECTION: Error - {e}")
+        
     # Called when SendMessage button is clicked
     def SendMessage(self):
         text = self.textMessage.get()
--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -1,48 +1,237 @@
+from Crypto.PublicKey import ECC
+from Crypto.Protocol.DH import key_agreement
+from Crypto.Hash import SHAKE128
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
+from Crypto.Signature import DSS
+import base64
+import json
+
 class Protocol:
+    
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
-    def __init__(self):
+    def __init__(self, shared_secret, callback_key = None, callback_send=None):
+        # Using ECC Diffe Hellman Key exchange instead of Diffe, relying on API documentation
+        self.shared_secret = shared_secret
         self._key = None
+        self.static_priv_key = ECC.generate(curve='p256')
+        self.ephemeral_priv_key = ECC.generate(curve='p256')
+        self.callback_key = callback_key
+        self.isConnectionSecure = False 
+        self.nonce = None
+        self.callback_send = callback_send
         pass
 
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
     # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
     def GetProtocolInitiationMessage(self):
-        return ""
+         nonce = get_random_bytes(16)
+         self.nonce = nonce
+         print("Initiation, send a static and an ephemeral public key to start the convo")
+         return {
+            "type": "init",
+            "nonce": base64.b64encode(nonce).decode('utf-8'),
+            "static_public_key": base64.b64encode(self.static_priv_key.public_key().export_key(format='PEM').encode()).decode('utf-8'),
+            "ephemeral_public_key": base64.b64encode(self.ephemeral_priv_key.public_key().export_key(format='PEM').encode()).decode('utf-8'),
+        }
+        # return {"type": "init", "public_key": self.dh.publickey().exportKey()}
 
 
     # Checking if a received message is part of your protocol (called from app.py)
-    # TODO: IMPLMENET THE LOGIC
+
     def IsMessagePartOfProtocol(self, message):
-        return False
+        print("checking the message")
+        return isinstance(message, dict) and 'type' in message
+        # return False
+
+    def sign_message(self, message):
+        signer = DSS.new(self.static_priv_key, 'fips-186-3')
+        hash_value = SHA256.new(message)
+        return signer.sign(hash_value)
+
+    def verify_signature(self, public_key, message, signature):
+        verifier = DSS.new(public_key, 'fips-186-3')
+        hash_value = SHA256.new(message)
+        try:
+            verifier.verify(hash_value, signature)
+            return True
+        except ValueError:
+            return False
 
 
     # Processing protocol message
     # TODO: IMPLMENET THE LOGIC (CALL SetSessionKey ONCE YOU HAVE THE KEY ESTABLISHED)
     # THROW EXCEPTION IF AUTHENTICATION FAILS
     def ProcessReceivedProtocolMessage(self, message):
-        pass
+        print("begining of Protocol Messages")
+        if message['type'] == 'init':
+            print("key exchange type")
+            # Process the Static Public Key
+            self.isConnectionSecure = True
+            static_public_key_b64 = message.get('static_public_key', '')
+            ephemeral_public_key_b64 = message.get('ephemeral_public_key', '')
+            extracted_nonce_b64 = message.get('nonce', '')
+            print("key part 1")
+
+            # Convert from base64 to binary
+            static_public_key_pem = base64.b64decode(static_public_key_b64)
+            ephemeral_public_key_pem = base64.b64decode(ephemeral_public_key_b64)
+            extracted_nonce = base64.b64decode(extracted_nonce_b64)
+            print("key part 2")
+            # Import the keys
+            peer_s_pub_key = ECC.import_key(static_public_key_pem)
+            peer_e_pub_key = ECC.import_key(ephemeral_public_key_pem)
+            print("key part 3")
+
+        if self._key is not None and self.callback_key is not None:
+            self.callback_key()
+
+            print("key part 4")
+        if peer_s_pub_key and peer_e_pub_key:
+            # ECDH key agreement
+            # Multiply your private key with the peer's public key
+            shared_secret_static = self.static_priv_key.d * peer_s_pub_key.pointQ
+            shared_secret_ephemeral = self.ephemeral_priv_key.d * peer_e_pub_key.pointQ
+            print("key part 5")
+            # You can concatenate both shared secrets and hash them to derive the session key
+            shared_secret = shared_secret_static.x.to_bytes() + shared_secret_ephemeral.x.to_bytes()
+            session_key = SHAKE128.new(shared_secret).read(32)
+            print("key part 6")
+            self.SetSessionKey(session_key)
+            if self.callback_key:
+                self.callback_key()
+            print("Key agreement successful")
+
+            response = self.CreateResponseMessage(extracted_nonce)
+            print("Response Message Creation successful")
+            self.requestSendMessage(response)
+            print("Response Message successful")
+        elif message['type'] == 'response':
+            server_static_pub_key_b64 = message.get('static_public_key', '')
+            server_ephemeral_pub_key_b64 = message.get('ephemeral_public_key', '')
+            
+            # Decode the server's public keys
+            server_static_pub_key = ECC.import_key(base64.b64decode(server_static_pub_key_b64))
+            server_ephemeral_pub_key = ECC.import_key(base64.b64decode(server_ephemeral_pub_key_b64))
+            
+            # Generate the shared secrets and derive the session key
+            shared_secret_static = self.static_priv_key.d * server_static_pub_key.pointQ
+            shared_secret_ephemeral = self.ephemeral_priv_key.d * server_ephemeral_pub_key.pointQ
+            
+            shared_secret = shared_secret_static.x.to_bytes() + shared_secret_ephemeral.x.to_bytes()
+            session_key = SHAKE128.new(shared_secret).read(32)
+            
+            # Set the session key
+            self.SetSessionKey(session_key)
+        else:
+            print("Error: Missing keys in the protocol message.")
+        
+        
+        
+    # Key Defining Function - Produce the keys heregiven an input
+        # if peer_s_pub_key and peer_e_pub_key:
+        #     # Key agreement and session key generation
+        #     self._key = key_agreement(
+        #         s_priv=self.static_priv_key,
+        #         static_pub=peer_s_pub_key,
+        #         eph_priv=self.ephemeral_priv_key,
+        #         eph_pub=peer_e_pub_key,
+        #         kdf=lambda x: SHAKE128.new(x).read(32)
+        #     )
+
+        #     self.SetSessionKey(self._key)  # Make sure this sets self._key correctly
+        #     if self.callback_key:
+        #         self.callback_key()
+        #     print("was able to get to key agreement")
+        # else:
+        #     print("Error: Missing keys in the protocol message.")
+            
+
+        # pass
+        print("got to the end")
 
 
     # Setting the key for the current session
     # TODO: MODIFY AS YOU SEEM FIT
     def SetSessionKey(self, key):
         self._key = key
+        if self._key:
+            self.isConnectionSecure = True 
         pass
 
-
+    def requestSendMessage(self, message):
+        if self.callback_send:
+         self.callback_send(message)
     # Encrypting messages
     # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
+    # Here, have it where it has the Client or the Server name iwthin the message to prevent reply attacks
     def EncryptAndProtectMessage(self, plain_text):
-        cipher_text = plain_text
-        return cipher_text
+        print("encrypting and protecting the message")
+        # cipher_text = plain_text
+        # return cipher_text
+        if self._key is None:
+            raise ValueError("Encryption key has not been initialized.")
+        AES_key = self._key
+        print("encrypting and protecting the message part 2")
+        if not isinstance(plain_text, bytes):
+            print("encrypting and protecting the message maybe?")
+            plain_text = plain_text.encode()
+        # Create/ initialize nonce here
+        # nonce = get_random_bytes(AES.block_size)
+        print("encrypting and protecting the message 3")
+        cipher = AES.new(AES_key, AES.MODE_GCM)
+        cipher_text, tag = cipher.encrypt_and_digest(plain_text)
+        # Do not need to encrypt nonce
+        print("encrypting and protecting the message 4")
+        return tag + cipher_text
+        # return plain_text.encode('utf-8')
 
+    def CreateResponseMessage(self, extracted_nonce):
+    # Create a new challenge and define the sender identity
+        new_challenge = get_random_bytes(16)
+        sender_id = "Reciever" 
 
+        # Create the response message
+        response_message = {
+            "encrypted_nonce": base64.b64encode(self.EncryptAndProtectMessage(extracted_nonce)).decode('utf-8'),
+            "new_challenge": base64.b64encode(new_challenge).decode('utf-8'),
+            "sender_id": sender_id,
+        }
+
+        # Encrypt the response message using the session key
+        encrypted_response = self.EncryptAndProtectMessage(json.dumps(response_message))
+
+        return {"encrypted_message": encrypted_response, 
+                "static_public_key": base64.b64encode(self.static_priv_key.public_key().export_key(format='PEM').encode()).decode('utf-8'),
+                "ephemeral_public_key": base64.b64encode(self.ephemeral_priv_key.public_key().export_key(format='PEM').encode()).decode('utf-8'),
+                }
+    
     # Decrypting and verifying messages
     # TODO: IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def DecryptAndVerifyMessage(self, cipher_text):
-        plain_text = cipher_text
-        return plain_text
+        print("we're decrypting now")
+        nonce = cipher_text[:AES.block_size]
+        print("we're decrypting now 2")
+        tag = cipher_text[AES.block_size:AES.block_size*2]
+        print("we're decrypting now 3")
+
+        encrypted = cipher_text[AES.block_size*2:]
+        print("we're decrypting now 4")
+        cipher = AES.new(self._key, AES.MODE_GCM, nonce=nonce)
+        print("we're decrypting now 5")
+
+        try:
+            # The decrypt_and_verify function will raise an exception if the verification fails
+            plain_text = cipher.decrypt_and_verify(encrypted, tag)
+            return plain_text.decode('utf-8')
+        except ValueError as e:
+            # Handle the error 
+            print(f"Error in decrypting and verifying message: {e}")
+            raise
+        # plain_text = cipher_text
+        # return plain_text
